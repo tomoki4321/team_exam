7aca19f0420f05cf8df0f9d3feb1a8c6e2b40d98
tomoki <benzousan7@gmail.com>
2022-12-20 16:34:12 +0900

    Teamのユーザーをオーナーもしくはそのユーザー自身のみ削除可能な機能に変更,またTeam編集をオーナーのみができるように変更

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,11 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  def edit
+    unless @team.owner == current_user
+      redirect_to @team, notice: I18n.t('views.messages.leader_only_edit')
+    end
+  end
 
   def create
     @team = Team.new(team_params)
